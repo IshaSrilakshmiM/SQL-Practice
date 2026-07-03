45a5dfd77e8af9485bfef0909a1e6ff83547c65d
Isha Srilakshmi M <ishamurugan06@gmail.com>
2026-07-03 15:17:11 +0530

    Added NOT Operator example.sql

---
+++ 07_NOT Operator.sql
@@ -0,0 +1,22 @@
+SELECT * FROM Customers
+WHERE CustomerName NOT LIKE 'A%';
+
+SELECT * FROM Customers
+WHERE CustomerID NOT BETWEEN 2 and 4 ;
+
+SELECT * FROM Customers 
+WHERE Country NOT IN ('UK');
+
+SELECT * FROM Customers
+WHERE NOT CustomerID > 50;
+
+SELECT * FROM Customers
+WHERE NOT CustomerId < 50;
+
+SELECT CustomerName
+FROM Customers
+WHERE NOT EXISTS (
+    SELECT *
+    FROM Orders
+    WHERE Customers.CustomerID = Orders.CustomerID
+);
