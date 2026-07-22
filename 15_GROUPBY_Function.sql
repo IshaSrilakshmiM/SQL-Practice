ea558e832bda9e6a203cb0bde4f9ac8d9cf3930b
Isha Srilakshmi M <ishamurugan06@gmail.com>
2026-07-22 11:07:39 +0530

    ADDED_GROUPBY_Function

---
+++ 15_GROUPBY_Function.sql
@@ -0,0 +1,19 @@
+SELECT CategoryID ,COUNT(*) AS total_avg
+FROM Products
+GROUP BY CategoryID;
+
+SELECT CategoryID ,AVG(Price) AS total_avg
+FROM Products
+GROUP BY CategoryID;
+
+SELECT CategoryID ,MIN(Price) AS total_min
+FROM Products
+GROUP BY CategoryID;
+
+SELECT CategoryID ,MAX(Price) AS total_max
+FROM Products
+GROUP BY CategoryID;
+
+SELECT CategoryID ,sum(Price) AS total_sum
+FROM Products
+GROUP BY CategoryID;
